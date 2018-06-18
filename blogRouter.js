032b58622f42a14ee57ddc39ac1fb5270846f9f0
oneEyedBunny <allyson.short@gmail.com>
2018-06-18 14:03:19 -0700

    Create modularization of routes
    Also WIP on PUT route

---
+++ blogRouter.js
@@ -0,0 +1,71 @@
+'use strict'
+
+//importing 3rd party libraries
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+// Modularize routes to /blog-posts
+const {BlogPosts} = require('./models');
+
+
+//Adding some blog post to test functionality
+BlogPosts.create("Cats are super", "BlahBlah1", "Cathy Caterson");
+BlogPosts.create("Dogs are great", "BlahBlah2", "Danny Doggery");
+BlogPosts.create("Bunnies are snuggly", "BlahBlah3", "Bonnie Bunyon");
+BlogPosts.create("Snakes are slithery", "BlahBlah4", "Seth Slitheran");
+
+
+//when this route is called, return the blog post
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+//when this route is called, returned blog is updated with user changes
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields =  ['title', 'content', 'author'];
+  for(let i = 0; i < requiredFields.length; i++) {
+    if(!(requiredFields[i] in req.body)) {
+      const errorMessage = (`Missing \`${requiredFields[i]}\` in request body`);
+      console.error(errorMessage);
+      return res.status(400).send(errorMessage);
+    }
+  }
+  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author)
+  res.status(201).json(item);
+});
+
+//when this route is called, returned blog is updated to remove the item specified
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog ${req.params.id}`);
+  res.status(204).end();
+});
+
+// when route is called,
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields =  ['id', 'title', 'content', 'author'];
+  for(let i = 0; i < requiredFields.length; i++) {
+    if(!(requiredFields[i] in req.body)) {
+      const errorMessage = (`Missing \`${requiredFields[i]}\` in request body`);
+      console.error(errorMessage);
+      return res.status(400).send(errorMessage);
+    }
+  }
+  if(req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post with blog id \`${req.params.id}\``);
+  BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  });
+  res.status(204).json(item);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,57 +4,19 @@
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
 
 // Modularize routes to /blog-posts
-const {BlogPosts} = require('./models');
+const blogRouter = require('./blogRouter');
 
 //creates the new express app, no need to server up static assets as there are none
 const app = express();
 
-
 //log the http layer
 app.use(morgan('common'));
 
-//Adding some blog post to test functionality
-BlogPosts.create("Cats are super", "BlahBlah1", "Cathy Caterson");
-BlogPosts.create("Dogs are great", "BlahBlah2", "Danny Doggery");
-BlogPosts.create("Bunnies are snuggly", "BlahBlah3", "Bonnie Bunyon");
-BlogPosts.create("Snakes are slithery", "BlahBlah4", "Seth Slitheran");
+//routing for landing page
+app.use('/blog-posts', blogRouter);
 
-
-//when this route is called, return the blog post
-app.get('/blog-posts', (req, res) => {
-  res.json(BlogPosts.get());
-});
-
-//when this route is called, returned blog is updated with user changes
-app.post('/blog-posts', jsonParser, (req, res) => {
-  const requiredFields =  ['title', 'content', 'author'];
-  for(let i = 0; i<requiredFields.length; i++) {
-    if(!(requiredFields[i] in req.body)) {
-      const errorMessage = (`Missing \`${requiredFields[i]}\` in request body`);
-      console.error(errorMessage);
-      return res.status(400).send(errorMessage);
-    }
-  }
-  const item = BlogPosts.create(req.body.title, req.body.content, req.body.author)
-  res.status(201).json(item);
-});
-
-//when this route is called, returned blog is updated to remove the item specified
-app.delete('/blog-posts/:id', (req, res) => {
-  BlogPosts.delete(req.params.id);
-  console.log(`Deleted blog ${req.params.id}`);
-  res.status(204).end();
-});
-
-// when route is called,
-// app.put('/blog-posts/:id', (req, res) => {
-//
-// });
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`)
