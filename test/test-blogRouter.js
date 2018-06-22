26096028aa5a645e49dc66f7eaedfe5961db901e
oneEyedBunny <allyson.short@gmail.com>
2018-06-21 22:53:55 -0700

    Add Chai dependencies for testing

--- test/test-blogRouter.js
+++ test/test-blogRouter.js
@@ -4,7 +4,7 @@ const chaiHttp = require('chai-http');
 
 //import
 const {app, runServer, closeServer} = require('../server');
-//const blogRouter = require('../blogRouter');
+const blogRouter = require('../blogRouter');
 
 //lets us use *expect & should* style syntax in tests
 const expect = chai.expect;
@@ -12,3 +12,23 @@ const should = chai.should();
 
 //lets us make http requests in our tests
 chai.use(chaiHttp);
+
+describe ('blogRouter', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  //normal test case for GET route
+  it('should get the blog on GET request', function() {
+    return chai.request(app)
+    .get('/blog-posts')
+    .then(function(res) {
+
+    });
+ })
+});
