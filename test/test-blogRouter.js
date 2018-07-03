66097f965320ac1bf348af3c09707630c3ccfc0e
oneEyedBunny <allyson.short@gmail.com>
2018-07-02 17:51:23 -0700

    Correct POST route test
    Adjusted name and added moment for date formatting.

--- test/test-blogRouter.js
+++ test/test-blogRouter.js
@@ -84,7 +84,7 @@ describe ('Blog Router', function() {
     });
   });
 
-  it.only('should return blogs with the correct fields', function() {
+  it('should return blogs with the correct fields', function() {
     let blogSample;
     return chai.request(app)
     .get('/blogs')
@@ -96,12 +96,9 @@ describe ('Blog Router', function() {
         expect(item).to.include.keys('id', 'title', 'content', 'author', 'created')
       })
       blogSample = res.body.blogPosts[0];
-      console.log("blogSample:", blogSample);
       return BlogPost.findById(blogSample.id)
     })
     .then(function(blog) {
-      console.log("blog:", blog);
-      /////////////how do we know that the same blog is going to be selected since we didnt specify [0]
       expect(blogSample.id).to.equal(blog.id);
       expect(blogSample.title).to.equal(blog.title);
       expect(blogSample.content).to.contain(blog.content);
@@ -109,11 +106,11 @@ describe ('Blog Router', function() {
       expect(moment(blogSample.created).format()).to.equal(moment(blog.created).format());
     })
   });
-}) //closes describe for GET
+})
 
   //normal test case for POST route
   describe('POST endpoints', function() {
-  it('should create a new blog post', function() {
+  it.only('should create a new blog post', function() {
     const newBlogPost = generateBlogData();
     return chai.request(app)
     .post('/blogs')
@@ -126,9 +123,7 @@ describe ('Blog Router', function() {
       expect(res.body.title).to.equal(newBlogPost.title);
       expect(res.body.content).to.equal(newBlogPost.content);
       expect(moment(res.body.created).format()).to.equal(moment(newBlogPost.created).format());
-      console.log("res.body = ", res.body.author);
-      console.log("newBlogPost = ", newBlogPost);
-      expect(res.body.author).to.equal(newBlogPost.firstName &" "& newBlogPost.lastName);
+      expect(res.body.author).to.equal(`${newBlogPost.author.firstName} ${newBlogPost.author.lastName}`);
       expect(res.body.id).to.not.be.null;
       return BlogPost.findById(res.body.id);
     })
@@ -136,55 +131,53 @@ describe ('Blog Router', function() {
       expect(blog.title).to.equal(newBlogPost.title);
       expect(blog.content).to.equal(newBlogPost.content);
       expect(moment(blog.created).format()).to.equal(moment(newBlogPost.created).format());
-      console.log("blog = ", blog.author);
-      console.log("newBlogPost = ", newBlogPost.fullName);
-      expect(blog.author).to.equal(newBlogPost.fullName);
+      expect(blog.fullName).to.equal(`${newBlogPost.author.firstName} ${newBlogPost.author.lastName}`);
     });
   });
-});//closes post endpoint test
-//
-//   //normal test case for DELETE route
-//   describe('DELETE endpoints', function() {
-//   it('should delete a specified blog post', function() {
-//     let selectedBlog;
-//     return BlogPost.findOne()
-//     .then(function(_selectedBlog) {
-//       selectedBlog = _selectedBlog;
-//       return chai.request(app)
-//       .delete(`/blogs/${selectedBlog.id}`);
-//     })
-//     .then(function(res) {
-//       expect(res).to.have.status(204);
-//       return BlogPost.findById(selectedBlog.id);
-//     })
-//       .then(function(_blog) {
-//           expect(_blog).to.be.null;
-//       });
-//   });
-//  }); //closes delete tests
+});
+
+  //normal test case for DELETE route
+  describe('DELETE endpoints', function() {
+  it('should delete a specified blog post', function() {
+    let selectedBlog;
+    return BlogPost.findOne()
+    .then(function(_selectedBlog) {
+      selectedBlog = _selectedBlog;
+      return chai.request(app)
+      .delete(`/blogs/${selectedBlog.id}`);
+    })
+    .then(function(res) {
+      expect(res).to.have.status(204);
+      return BlogPost.findById(selectedBlog.id);
+    })
+      .then(function(_blog) {
+          expect(_blog).to.be.null;
+      });
+  });
+ }); //closes delete tests
 
   //normal test case for PUT route
- //  describe('PUT endpoint', function() {
- //  it('should update a specified blog post', function() {
- //    let selectedBlog;
- //    return BlogPost.findOne()
- //    .then(function(_selectedBlog) {
- //      selectedBlog = _selectedBlog;
- //      console.log("my blog BEFORE edit is", selectedBlog);
- //      selectedBlog.title = faker.lorem.words(),
- //      console.log("my blog AFTER edit is", selectedBlog);
- //      return chai.request(app)
- //        .put(`/blogs/${selectedBlog.id}`)
- //        .send(selectedBlog)
- //      })
- //      .then(function(res) {
- //        expect(res).to.have.status(200);
- //        return BlogPost.findById(selectedBlog.id);
- //      })
- //      .then(function(blog) {
- //        expect(blog.body.title).to.equal(selectedBlog.title);
- //        expect(blog.body.content).to.equal(selectedBlog.content);
- //      })
- //    });
- // }); //closes PUT test case
+  describe('PUT endpoint', function() {
+  it('should update a specified blog post', function() {
+    let selectedBlog;
+    return BlogPost.findOne()
+    .then(function(_selectedBlog) {
+      selectedBlog = _selectedBlog;
+      console.log("my blog BEFORE edit is", selectedBlog);
+      selectedBlog.title = faker.lorem.words(),
+      console.log("my blog AFTER edit is", selectedBlog);
+      return chai.request(app)
+        .put(`/blogs/${selectedBlog.id}`)
+        .send(selectedBlog)
+      })
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        return BlogPost.findById(selectedBlog.id);
+      })
+      .then(function(blog) {
+        expect(blog.body.title).to.equal(selectedBlog.title);
+        expect(blog.body.content).to.equal(selectedBlog.content);
+      })
+    });
+ }); //closes PUT test case
 });//closes whole test set
